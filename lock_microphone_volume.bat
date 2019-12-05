6b6e13a253eae7303db748b3c81d2421be83c550
neo <24514585+neo0nymous@users.noreply.github.com>
2019-12-05 19:21:20 +0100

    Ah yes

---
+++ lock_microphone_volume.bat
@@ -0,0 +1,1 @@
+nircmdc.exe loop 172800 500 setsysvolume 52536 default_record
---
+++ start_lock_microphone_volume.bat
@@ -0,0 +1,4 @@
+hide_cmd_window.vbs lock_microphone_volume.bat
+taskkill /F /IM cmd.exe
+taskkill /F /IM conhost.exe /T
+exit
